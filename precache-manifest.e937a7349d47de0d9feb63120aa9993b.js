10b1438f492f283e6d0e478c4ace35cae10f4a5c
vi_mi <fenil.jain2018@vitstudent.ac.in>
2019-12-31 10:18:36 +0530

    Updates

---
+++ precache-manifest.e937a7349d47de0d9feb63120aa9993b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffd5e03d2a8360bb5d2664656859f2d",
+    "url": "/translate/index.html"
+  },
+  {
+    "revision": "649057dc2cb34bb36db1",
+    "url": "/translate/static/css/main.5ecb2b11.chunk.css"
+  },
+  {
+    "revision": "5141c999c8bb50fb960b",
+    "url": "/translate/static/js/2.c9fed3f7.chunk.js"
+  },
+  {
+    "revision": "28b65448ecbc8d066152ecb3276e74ce",
+    "url": "/translate/static/js/2.c9fed3f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "649057dc2cb34bb36db1",
+    "url": "/translate/static/js/main.a0bba76a.chunk.js"
+  },
+  {
+    "revision": "91f79ecda24204526ce0",
+    "url": "/translate/static/js/runtime-main.b181b9ec.js"
+  },
+  {
+    "revision": "fa831c18394a639f325f19ddf4bee2b7",
+    "url": "/translate/static/media/3260107.fa831c18.jpg"
+  }
+]);
