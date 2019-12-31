52fe2249e8806e714c026b5d3122fe5fc2eb7c42
vi_mi <fenil.jain2018@vitstudent.ac.in>
2019-12-31 10:05:56 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,229 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cookie from 'react-cookies';
+//this.state.disabled
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(){
+    super();
+    this.state={
+      text: "",
+      translation: "",
+      loading: false,
+      translation_list: "",
+      disabled: true
+    };
+  }
+
+  handleSpecialChar=(event)=>{
+    var words=event.target.value.split(" ");
+      //console.log(words);
+      //console.log(words[words.length-2]);
+      words[words.length-2]=this.state.translation;
+      //console.log(this.state.translation);
+      //console.log(words.join(" "));
+      this.setState({
+        text: words.join(" ")
+      });
+      document.getElementById("textarea").value=words.join(" ");//+" "
+  }
+
+  componentWillMount(){
+    cookie.save('translateWebsite', JSON.stringify({
+      "lang": "hindi"
+    }));
+  }
+
+  /*keyRedirect=()=>{
+     this.changeText()
+  }*/
+
+  langChange=(event)=>{
+    console.log(event.target.value);
+    cookie.save('translateWebsite', JSON.stringify({
+      "lang": event.target.value
+    }));
+  }
+
+  translateWordChange=(event)=>{
+    //console.log(event);
+    //console.log(event.target.value);
+    this.setState({
+      translation: event.target.value
+    });
+    var words=this.state.text.split(" ");
+    //console.log(words);
+    //console.log(words[words.length-2]);
+    words[words.length-1]=event.target.value;
+    //console.log(this.state.translation);
+    //console.log(words.join(" "));
+    this.setState({
+      text: words.join(" ")
+    });
+    document.getElementById("textarea").value=words.join(" ")+" ";//+" " 
+    document.getElementById("textarea").focus();
+  }
+
+  changeText=(event)=>{
+    //console.log(document.getElementById("textarea").value);
+    event.target.value=document.getElementById("textarea").value;
+    this.setState({
+      text: event.target.value
+    });
+    if(event.keyCode===8)
+     {
+       console.log("Backspace");
+       console.log(event.key);
+       return;
+     }
+    if(!this.state.disabled)
+     {
+       return;
+     }
+    var lastWord=event.target.value.split(" ").slice(-1)[0];
+    var lastChar=lastWord.slice(-1);
+    /*if(lastChar===',' || lastChar===',' || lastChar==='?' || lastChar==='!')
+     {
+      this.handleSpecialChar(event);
+      return;
+     }*/
+    //console.log(event.target.value.split(" ").slice(-1)[0].slice(-1));
+    //console.log(event.target.value.split(" ").slice(-1)[0]);
+    /*if(event.target.value)
+     {
+
+     }*/
+    //console.log(event.target.value.split(" "));
+    //console.log("lastChar: "+lastChar);
+    if(lastWord!=='')//  && lastChar!==',' && lastChar!=='.' && lastChar!=='?' && lastChar!=='!'// && lastChar===',' && lastChar===',' && lastChar==='?' && lastChar==='!'
+     {
+       //console.log(lastWord);
+       /*if(lastChar===',' || lastChar==='.' || lastChar==='?' || lastChar==='!')
+        {
+          console.log("Kuch to karna padega!");
+          console.log(event.target.value.split(lastChar));
+        }*/
+       this.setState({
+        loading: true,
+        disabled: false
+      });
+      console.log("Sending Request!");
+       fetch(`http://146.148.85.67/processWordJSON?inString=${lastWord}&lang=${cookie.load('translateWebsite').lang}`,{
+        //fetch('https://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=tamil&inString=namast',{
+          method: "get",
+          /*headers: {
+              //"Access-Control-Allow-Origin": "*",
+              //'Content-type':'text/html;charset=utf-8'
+          }*/
+      })
+      .then((resp)=>{
+        if(resp.status===200)
+        {
+          this.setState({
+            loading: false,
+            disabled: true
+          });
+          document.getElementById("textarea").focus();
+        }
+        return resp;
+      })
+      .then(response => response.json())
+      .then(data=>{
+        console.log(data);
+        this.setState({
+          translation: data.itrans
+        });
+        this.setState({
+          translation_list: data.twords[0].options
+        });
+        //console.log(data.twords[0].optmap);
+        document.getElementById("textarea").value=this.state.text;//+" "
+      }).catch((err)=>{
+        console.log(err);
+      });
+     }
+    else{
+      /*if(lastChar==="") 
+       {
+        console.log("Last Space: "+lastChar);   
+       }
+      else{
+        console.log("Last Char: "+lastChar);
+      }*/
+      //document.getElementById("textarea").value=this.state.text;//+" "
+      var words=event.target.value.split(" ");
+      //console.log(words);
+      //console.log(words[words.length-2]);
+      words[words.length-2]=this.state.translation;
+      //console.log(this.state.translation);
+      //console.log(words.join(" "));
+      this.setState({
+        text: words.join(" ")
+      });
+      document.getElementById("textarea").value=words.join(" ");//+" " 
+      /*if(lastChar!==" " && lastChar!=="")
+       {
+          console.log(words.join(" "));
+          document.getElementById("textarea").value=words.join(" ");//+" " 
+       }
+      else
+       {
+          document.getElementById("textarea").value=words.join(" ");//+" " 
+       }*/
+    }
+    //console.log("Last Word:"+event.target.value.split(" ")[-1]);
+  }
+
+  render(){
+    return (
+      <div className="main-container"><div>
+            <div className="langOption-container">
+                <button type="button" className="btn-lang" onClick={this.langChange} value="hindi">Hindi</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="tamil">Tamil</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="telugu">Telugu</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="bengali">Bengali</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="gujarati">Gujarati</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="marathi">Marathi</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="kannada">Kannada</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="malayalam">Malayalam</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="punjabi">Punjabi</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="nepali">Nepali</button>
+            </div>
+            <div className="textbox-container">
+                <textarea className="textbox" id="textarea" disabled={false} rows="10" cols="60" onKeyUp={(event)=>{
+                  this.changeText(event);
+                }} autoFocus></textarea>
+                {/*Namastey Duniyaa!*/}
+            </div>
+            <div className="translation-container">
+              <div>
+                {
+                  this.state.loading
+                  ?<div>
+                    <p><button type="button" className="btn-translation">Loading...</button></p>
+                  </div>
+                  :<div>
+                      {/*<p>Namastey Duniyaa!</p>*/}
+                      <p>{this.state.translations}</p>
+                      {
+                        this.state.translation_list!==""?this.state.translation_list.map((data)=>{
+                          //console.log(data);
+                          return(
+                            <button type="button" id={data} className="btn-translation" value={data} onClick={this.translateWordChange}>{data}</button>
+                          );
+                        })
+                        :<div></div>
+                      }
+                   </div>
+                }
+              </div>
+            </div>
+          </div>
+          
+      </div>
+    );
+  }
 }
 
 export default App;
+/*<a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by freepik - www.freepik.com</a>*/
---
+++ src/App1.js
@@ -0,0 +1,374 @@
+import React from 'react';
+import './App1.css';
+import cookie from 'react-cookies';
+//this.state.disabled
+class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state={
+      text: "",
+      translation: "",
+      loading: false,
+      translation_list: "",
+      disabled: false
+    };
+  }
+
+  replaceAt=(string, index, replacement)=>{
+    return string.substr(0, index) + replacement+ string.substr(index + replacement.length);
+  }
+
+  // handleSpecialChar=(event)=>{
+  //   var words=event.target.value.split(" ");
+  //     //console.log(words);
+  //     //console.log(words[words.length-2]);
+  //     words[words.length-2]=this.state.translation;
+  //     //console.log(this.state.translation);
+  //     //console.log(words.join(" "));
+  //     this.setState({
+  //       text: words.join(" ")
+  //     });
+  //     document.getElementById("textarea").value=words.join(" ");//+" "
+  // }
+
+  componentWillMount(){
+    cookie.save('translateWebsite', JSON.stringify({
+      "lang": "hindi"
+    }));
+  }
+
+  /*keyRedirect=()=>{
+     this.changeText()
+  }*/
+
+  langChange=(event)=>{
+    console.log(event.target.value);
+    cookie.save('translateWebsite', JSON.stringify({
+      "lang": event.target.value
+    }));
+  }
+
+  fetchCall=(a, disable)=>{
+    //Promise pr((){});
+    //console.log("Sending Request For: "+a);
+    if(disable)
+     {
+       this.setState({
+         disabled: true
+       });
+     }
+    return new Promise((resolve, reject)=>{
+      fetch(`http://146.148.85.67/processWordJSON?inString=${a}&lang=${cookie.load('translateWebsite').lang}`,{
+        //fetch('https://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=tamil&inString=namast',{
+          method: "get",
+      })
+      .then((resp)=>{
+        if(resp.status===200)
+        {
+          this.setState({
+            loading: false
+          });
+          document.getElementById("textarea").focus();
+        }
+        return resp;
+      })
+      .then(response => response.json())
+      .then(data=>{
+        console.log(data);
+        this.setState({
+          translation: data.itrans,
+          
+        });
+        this.setState({
+          translation_list: data.twords[0].options
+        });
+        if(disable)
+         {
+           this.setState({
+            disabled: false
+           });
+           //console.log("As Disabled:"+data.itrans);
+         }
+        resolve();
+        //document.getElementById("textarea").value=this.state.text;//+" "
+      }).catch((err)=>{
+        console.log(err);
+        this.setState({
+          disabled: false
+         });
+         //alert("Error!");
+        reject();
+      });
+    });
+  }
+
+  translate = async (event)=>{
+    //console.log(event.key);
+    //console.log(String.fromCharCode(event.keyCode));
+    //if((event.key>='a' && event.key<='z') || (event.key>='A' && event.key<='Z'))
+    if(event.key.length===1 && ((event.key>='a' && event.key<='z') || (event.key>='A' && event.key<='Z')))
+     {
+       //console.log(event.key);
+       //console.log(this.state.text+event.key);
+       this.setState({
+        text: this.state.text+event.key
+      });
+      //var lastWord="";
+      console.log("(1)Sending Request for: "+this.state.text+event.key);
+      this.fetchCall(this.state.text+event.key);
+      /*fetch(`http://146.148.85.67/processWordJSON?inString=${this.state.text+event.key}&lang=${cookie.load('translateWebsite').lang}`,{
+        //fetch('https://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=tamil&inString=namast',{
+          method: "get",
+      })
+      .then((resp)=>{
+        if(resp.status===200)
+        {
+          this.setState({
+            loading: false,
+            disabled: true
+          });
+          document.getElementById("textarea").focus();
+        }
+        return resp;
+      })
+      .then(response => response.json())
+      .then(data=>{
+        console.log(data);
+        this.setState({
+          translation: data.itrans
+        });
+        this.setState({
+          translation_list: data.twords[0].options
+        });
+        //document.getElementById("textarea").value=this.state.text;//+" "
+      }).catch((err)=>{
+        console.log(err);
+      });*/
+     }
+    else if(event.key===' ' || event.key===',' || event.key==='.' || event.key==='!' || event.key==='?')
+     {
+       var specialChar=event.key;
+       console.log("Setting the word");
+       this.setState({
+         text: "",
+         disabled: true
+       });
+       var something=document.getElementById("textarea").value;
+       var textValue=document.getElementById("textarea").value;
+       //something[something.lenth-1=" ";
+       something=this.replaceAt(something, something.length-1, " ");
+       console.log(something);
+       //textValue=textValue.substr(0, textValue.length-1).split(" ");
+       //console.log(textValue.splice(-1));
+       //console.log(this.state.translation);
+       //console.log(something.split(" "));
+       //console.log(something.split(" ").length);
+       //console.log(something.split(" ")[something.split(" ").length-2]);
+       console.log("(2)Sending Request for: "+something.split(event.key)[something.split(event.key).length-1]);
+       await this.fetchCall(something.split(" ")[something.split(" ").length-2], true);
+       await console.log(this.state.translation);
+       //something.split(" ")[something.split(" ").length-2]=this.state.translation;
+       var a=something.split(" ")[something.split(" ").length-2].length+1;
+       //something.slice(-a)="";
+       console.log(something.length-a);//something.slice(-a)
+       console.log(something.length-2);
+       console.log(something.substr(0, something.length-a)+this.state.translation+" ");
+       console.log(specialChar);
+       if(specialChar!==" ")
+        {
+          document.getElementById("textarea").value=something.substr(0, something.length-a)+this.state.translation+specialChar+" ";
+        }
+      else{
+        document.getElementById("textarea").value=something.substr(0, something.length-a)+this.state.translation+" ";
+      }
+       /*setTimeout(this.setState({
+         disabled: false
+       }), 100);*/
+     }
+    else if(event.key==="Backspace")
+     {
+       var textValue=document.getElementById("textarea").value;
+       console.log(textValue[textValue.length]);
+       if(textValue[textValue.length-1]===" " || textValue[textValue.length-1]==="," || textValue[textValue.length-1]==="." || textValue[textValue.length-1]==="?" || textValue[textValue.length-1]==="!")
+        {
+          return;
+        }
+       //console.log("Backing Space");
+       //console.log(this.state.text.substr(0, this.state.text.length-1));
+       //console.log(document.getElementById("textarea").value);
+       this.setState({
+         text: document.getElementById("textarea").value//this.state.text.substr(0, this.state.text.length-1)
+       });
+       //console.log("Sending Request for: "+textValue);
+       //console.log(textValue[textValue.length-1]);
+       textValue=textValue.substr(0, textValue.length).split(" ").splice(-1);
+       //console.log("Splitted Input:"+textValue.substr(0, textValue.length).split(" "));
+       //console.log("Sending Request for: "+document.getElementById("textarea").value);
+       //this.fetchCall(document.getElementById("textarea").value);
+       console.log("(3)Sending Request for: "+textValue);
+       this.fetchCall(textValue);
+       /*fetch(`http://146.148.85.67/processWordJSON?inString=${document.getElementById("textarea").value}&lang=${cookie.load('translateWebsite').lang}`,{
+        //fetch('https://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=tamil&inString=namast',{
+          method: "get",
+      })
+      .then((resp)=>{
+        if(resp.status===200)
+        {
+          this.setState({
+            loading: false,
+            disabled: true
+          });
+          document.getElementById("textarea").focus();
+        }
+        return resp;
+      })
+      .then(response => response.json())
+      .then(data=>{
+        console.log(data);
+        this.setState({
+          translation: data.itrans
+        });
+        this.setState({
+          translation_list: data.twords[0].options
+        });
+        //document.getElementById("textarea").value=this.state.text;//+" "
+      }).catch((err)=>{
+        console.log(err);
+      });*/
+     }
+  }
+
+  translateWordChange=(event)=>{
+    //console.log(event);
+    //console.log(event.target.value);
+    this.setState({
+      translation: event.target.value
+    });
+    var words=document.getElementById("textarea").value.split(" ");
+    words[words.length-1]=event.target.value;
+    this.setState({
+      text: words.join(" ")
+    });
+    console.log(words.join(" "));
+    document.getElementById("textarea").value=words.join(" ")+" ";
+    document.getElementById("textarea").focus();
+  }
+
+  changeText=(event)=>{
+    event.target.value=document.getElementById("textarea").value;
+    this.setState({
+      text: event.target.value
+    });
+    if(event.keyCode===8)
+     {
+       console.log("Backspace");
+       console.log(event.key);
+       return;
+     }
+    if(!this.state.disabled)
+     {
+       return;
+     }
+    var lastWord=event.target.value.split(" ").slice(-1)[0];
+    var lastChar=lastWord.slice(-1);
+    if(lastWord!=='')
+     {
+       this.setState({
+        loading: true,
+        disabled: false
+      });
+      console.log("Sending Request!");
+       fetch(`http://146.148.85.67/processWordJSON?inString=${lastWord}&lang=${cookie.load('translateWebsite').lang}`,{
+        //fetch('https://xlit.quillpad.in/quillpad_backend2/processWordJSON?lang=tamil&inString=namast',{
+          method: "get",
+      })
+      .then((resp)=>{
+        if(resp.status===200)
+        {
+          this.setState({
+            loading: false,
+            disabled: true
+          });
+          document.getElementById("textarea").focus();
+        }
+        return resp;
+      })
+      .then(response => response.json())
+      .then(data=>{
+        console.log(data);
+        this.setState({
+          translation: data.itrans
+        });
+        this.setState({
+          translation_list: data.twords[0].options
+        });
+        document.getElementById("textarea").value=this.state.text;//+" "
+      }).catch((err)=>{
+        console.log(err);
+      });
+     }
+    else{
+      var words=event.target.value.split(" ");
+      words[words.length-2]=this.state.translation;
+      this.setState({
+        text: words.join(" ")
+      });
+      document.getElementById("textarea").value=words.join(" ");
+    }
+  }
+
+  render(){
+    return (
+      <div className="main-container"><div>
+            <div className="langOption-container">
+                <button type="button" className="btn-lang" onClick={this.langChange} value="hindi">Hindi</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="tamil">Tamil</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="telugu">Telugu</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="bengali">Bengali</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="gujarati">Gujarati</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="marathi">Marathi</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="kannada">Kannada</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="malayalam">Malayalam</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="punjabi">Punjabi</button>
+                <button type="button" className="btn-lang" onClick={this.langChange} value="nepali">Nepali</button>
+            </div>
+            <div className="textbox-container">
+                <textarea className="textbox" id="textarea" disabled={this.state.disabled} rows="10" cols="60" onKeyUp={(event)=>{
+                  //this.changeText(event);
+                  this.translate(event);                
+                }} autoFocus></textarea>
+                {/*Namastey Duniyaa!*/}
+            </div>
+            <div className="translation-container">
+              <div>
+                {
+                  this.state.loading
+                  ?<div>
+                    <p><button type="button" className="btn-translation">Loading...</button></p>
+                  </div>
+                  :<div>
+                      {/*<p>Namastey Duniyaa!</p>*/}
+                      <p>{this.state.translations}</p>
+                      {
+                        this.state.translation_list!==""?this.state.translation_list.map((data)=>{
+                          //console.log(data);
+                          return(
+                            <button type="button" id={data} className="btn-translation" value={data} onClick={this.translateWordChange}>{data}</button>
+                          );
+                        })
+                        :<div></div>
+                      }
+                   </div>
+                }
+              </div>
+            </div>
+          </div>
+          
+      </div>
+    );
+  }
+}
+
+export default App;
+/*<a href="https://www.freepik.com/free-photos-vectors/background">Background vector created by freepik - www.freepik.com</a>*/
